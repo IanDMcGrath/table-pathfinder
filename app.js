bad3e33eba5f41b18c4ee16c2749092d151cccb2
IanDMcGrath <ian92415@gmail.com>
2022-03-02 20:42:55 -0700

    working sql from statements

--- app.js
+++ app.js
@@ -1,26 +1,71 @@
-import { Database, Table, Column } from "./db";
+const db = require("./db.js");
+const { Database, Table, Column, Graph } = db;
+// import { Database, Table, Column } from "./db";
+
 
 const db1 = new Database('db1');
 const t1 = new Table('personnel');
 const t2 = new Table('stations');
 const t3 = new Table('stationworkers');
 
-db1.addTable(t1);
-db1.addTable(t2);
-db1.addTable(t3);
+const match1 = 'personnelToStationWorkers'
+const match2 = 'stationsToStationWorkers'
 
-t1.addColumn('id', 'int');
+db1.addTable(t1);
+t1.addColumn('id', 'int', match1);
 t1.addColumn('name', 'string');
 t1.addColumn('email', 'string');
 t1.addColumn('address', 'string');
 t1.addColumn('phone', 'string');
 
-t2.addColumn('id', 'int');
+db1.addTable(t2);
+t2.addColumn('id', 'int', match2);
 t2.addColumn('name', 'string');
 t2.addColumn('address', 'string');
 
+db1.addTable(t3);
 t3.addColumn('id', 'int');
-t3.addColumn('personnelId', 'int');
-t3.addColumn('stationId', 'int');
+t3.addColumn('personnelId', 'int', match1);
+t3.addColumn('stationId', 'int', match2);
 
-console.log(db1.getTables());
+// console.log(db1.getTablesColumns());
+// t3.columns.personnelId.getPath();
+
+
+
+const g = new Graph();
+
+// g.addVertex("A"); // table
+// g.addVertex("B");
+// g.addVertex("C");
+// g.addVertex("D");
+// g.addVertex("E");
+// g.addVertex("F");
+// g.addVertex("G");
+
+Object.values(db1.tables).forEach(t => {
+  g.addTable(t.name);
+});
+
+t1.addJoin(t3, t1.columns.id, t3.columns.personnelId);
+t2.addJoin(t3, t2.columns.id, t3.columns.stationId);
+
+g.addJoin(t1.name, t3.name);
+g.addJoin(t2.name, t3.name);
+
+g.tablesData = db1.tables;
+
+let r = g.bfs("personnel", "stations");
+console.log(r.path[1]);
+
+// g.addEdge("A","B"); // keys
+// g.addEdge("A","C");
+// g.addEdge("A","D");
+// g.addEdge("B","C");
+// g.addEdge("B","D");
+// g.addEdge("C","D");
+// g.addEdge("C","E");
+// g.addEdge("D","F");
+// g.addEdge("F","G");
+
+// console.log(g.bfs("G", "A"));
--- db.js
+++ db.js
@@ -20,7 +20,7 @@ normal use -
   present to user
 */
 
-export class Database {
+class Database {
   // use database like a folder directory
   // a Database might have a parent database
   /*
@@ -36,6 +36,7 @@ export class Database {
   */
   constructor(name) {
     this.name = name;
+    this.tables = {};
   }
 
   getPath() {
@@ -55,18 +56,34 @@ export class Database {
 
   addTable(table) {
     this.tables[table.name] = table;
+    table.parent = this;
   }
 
   getTables() {
-    return this.tables;
+    let result = {};
+    Object.values(this.tables).forEach(t => {
+      let columns = Object.keys(t.getColumns());
+      result[t.name] = columns;
+    });
+    return result;
+  }
+
+  getTablesColumns() {
+    let result = {};
+    Object.values(this.tables).forEach(t => {
+      let columns = t.getColumns();
+      result[t.name] = columns;
+    });
+    return result;
   }
 }
 
-export class Table {
+class Table {
   // parent db : { table1: {name: '', parent: Database, columns: { col1: Column, ... } } }
   constructor(name) {
     this.name = name;
     this.columns = {};
+    this.match = {};
   }
 
   getPath() {
@@ -77,20 +94,27 @@ export class Table {
     }
   }
 
-  addColumn(column) {
-    //
-    this.columns[column.name] = column;
+  addColumn(name, dataType, match='') {
+    this.columns[name] = new Column(name, this, dataType, match);
   }
 
   getColumns() {
-    return this.columns;
+    return Object.values(this.columns).map(col => col.getColumn());
+  }
+
+  addJoin(table2, col1, col2) { // table1.addMatch(table 2, column 1, column 2) // FROM t1 JOIN t2 ON col1 = col2
+    this.match[table2.name] = {table: table2, column: col2};
+    table2.match[this.name] = {table: this, column: col1};
   }
 }
 
-export class Column {
+class Column {
   // parent table : { col1: {name: '', parent: Table, dataType: '' } }
-  constructor(name) {
+  constructor(name, parentTable, dataType, joinId='') {
     this.name = name;
+    this.parent = parentTable;
+    this.dataType = dataType;
+    this.joinId = joinId; // the grouping (string) that tables' columns will use to be matched
   }
 
   getPath() {
@@ -100,5 +124,168 @@ export class Column {
       console.log('getPath() failed, either no name or no parent');
     }
   }
+
+  getColumn() {
+    return {[this.name]: this.dataType};
+  }
 }
 
+class Graph { // ORIG
+  constructor() {
+    this.tables = [];
+    this.joins = {};
+    this.numJoins = 0;
+    this.tablesData = {};
+  }
+
+  addTable(v) {
+    this.tables.push(v);
+    this.joins[v] = [];
+  }
+
+  addJoin(v, w) {
+    this.joins[v].push(w);
+    this.joins[w].push(v);
+    this.numJoins++;
+  }
+
+  bfs(goal, root = this.tables[0]) {
+    let join = this.joins;
+    const queue = [root];
+    const joins = [];
+    const found = [];
+    joins[root] = 0;
+
+    const pathfind = (goal, root, found) => {
+      const stack = [goal];
+
+      let u = found[goal];
+      while(u != root) {
+        stack.push(u);
+        u = found[u];
+      }
+      stack.push(root);
+      let shortPath = stack.reverse().join(' <-> ');
+      let longPath = stack.reverse();
+      let lines = ['FROM ' + longPath[0]];
+      longPath = lines.concat(longPath.map((n, i) => {
+        if (i+1 < longPath.length) {
+          let t1 = this.tablesData[n];
+          let t2 = this.tablesData[longPath[i+1]];
+          // console.log(`t1 name = ${t1.name}`);
+          // console.log(`t1 name = ${t2.name}`);
+          // console.log(t1.match[t2.name].table.name);
+          // console.log(t2.match[t1.name].table.name);
+          let c1 = t2.match[t1.name].column;
+          let c2 = t1.match[t2.name].column;
+          return 'JOIN ' + t2.getPath() + ' ON ' + c1.getPath() + ' = ' + c2.getPath();
+        }
+      }));
+      return [shortPath, longPath.join('\n')];
+    }
+
+
+    while(queue.length) {
+      let t = queue.shift();
+
+      if (t === goal) {
+        return {
+          distance: joins[goal],
+          path: pathfind(goal, root, found)
+        };
+      }
+
+      for (let i = 0; i < join[t].length; i++) {
+        if (!found[join[t][i]]) {
+          queue.push(join[t][i]);
+          joins[join[t][i]] = joins[t] + 1;
+          found[join[t][i]] = t;
+        }
+      }
+    }
+
+    return false;
+  }
+}
+
+// class Graph {
+//   constructor() {
+//     this.tables = {};
+//     this.joinKeys = {};
+//     this.numJoins = 0;
+//   }
+
+//   addVertex(table) {
+//     this.tables[table.name] = table;
+//     this.joins[table.name] = {};
+//   }
+
+//   addEdge(col1, col2) {
+//     this.joins[col1.name] = col2;
+//     this.joins[col2].push(col1);
+//     this.numJoins++;
+//   }
+
+//   bfs(goal, root = this.tables[0]) {
+//     let join = this.joins;
+
+//     const queue = [];
+//     queue.push(root);
+
+//     const bFound = [];
+//     bFound[root] = true;
+
+//     const numJoins = [];
+//     numJoins[root] = 0;
+
+//     const found = [];
+//     found[root] = null;
+
+//     const buildPath = (goal, root, found) => {
+//       const stack = [];
+//       stack.push(goal);
+
+//       let u = found[goal];
+
+//       while(u != root) {
+//         stack.push(u);
+//         u = found[u];
+//       }
+
+//       stack.push(root);
+
+//       let path = stack.reverse().join('-');
+
+//       return path;
+//     }
+
+
+//     while(queue.length) {
+//       let v = queue.shift();
+
+//       if (v === goal) {
+//         return {
+//           distance: numJoins[goal],
+//           path: buildPath(goal, root, found)
+//         };
+//       }
+
+//       for (let i = 0; i < join[v].length; i++) {
+//         if (!bFound[join[v][i]]) {
+//           bFound[join[v][i]] = true;
+//           queue.push(join[v][i]);
+//           numJoins[join[v][i]] = numJoins[v] + 1;
+//           found[join[v][i]] = v;
+//         }
+//       }
+//     }
+
+//     return false;
+//   }
+
+// }
+
+module.exports.Database = Database;
+module.exports.Table = Table;
+module.exports.Column = Column;
+module.exports.Graph = Graph;
